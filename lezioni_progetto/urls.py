afe71538038a1c0026a9615e0431bc836712478d
Andrea Manzi <manzolo@libero.it>
2025-04-26 15:37:53 +0200

    Init

--- lezioni_progetto/urls.py
+++ lezioni_progetto/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('lezioni/', include('lezioni.urls')),
+    path('', include('lezioni.urls')),
+#    path('lezioni/', include('lezioni.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

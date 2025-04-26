0f18fc1ab75d1ddb403f21edc785cd978d298a78
Andrea Manzi <manzolo@libero.it>
2025-04-26 16:29:12 +0200

    Init

--- lezioni/views.py
+++ lezioni/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Lezione
 
 def lista_lezioni(request):
-    lezioni_ordinate = Lezione.objects.all().order_by('numero')
-    return render(request, 'lezioni/lista_lezioni.html', {'lezioni': lezioni_ordinate})
+    lezioni = Lezione.objects.all().order_by('numero')
+    context = {'lezioni': lezioni}
+    return render(request, 'lezioni/lista_lezioni.html', context)
 
 def dettaglio_lezione(request, lezione_id):
     lezione = get_object_or_404(Lezione, pk=lezione_id)

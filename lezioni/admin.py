bf2394b68e5ec2337b08ca70505778b2a614d2d0
Andrea Manzi <manzolo@libero.it>
2025-04-27 11:38:34 +0200

    Aggiunti docenti e corretto stile css per footer

--- lezioni/admin.py
+++ lezioni/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Lezione
+from .models import Lezione, Docente
 
+# Registra il modello Docente
+admin.site.register(Docente)
+
+# Registra il modello Lezione
 admin.site.register(Lezione)
--- lezioni/models.py
+++ lezioni/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+class Docente(models.Model):
+    nome = models.CharField(max_length=100)
+    cognome = models.CharField(max_length=100)
+    nominativo = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.nome} {self.cognome}"
+
 class Lezione(models.Model):
     numero = models.IntegerField()
     titolo = models.CharField(max_length=200)
@@ -8,6 +16,7 @@ class Lezione(models.Model):
     file_slide = models.FileField(upload_to='documenti/slide/', null=True, blank=True)
     file_domande = models.FileField(upload_to='documenti/domande/', null=True, blank=True)
     file_risposte = models.FileField(upload_to='documenti/risposte/', null=True, blank=True)
+    docente = models.ForeignKey(Docente, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.titolo

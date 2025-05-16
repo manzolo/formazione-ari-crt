e313ae4c7e4121ae929568402fa5449ceba42b9c
Andrea Manzi <manzolo@libero.it>
2025-05-16 23:48:47 +0200

    Postgres

--- lezioni_progetto/settings.py
+++ lezioni_progetto/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'lezioni_progetto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db/db.sqlite3',
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
